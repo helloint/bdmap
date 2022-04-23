3935439891e3cf21fde0b12a2764df0d14ba154d
Wayne Mao <helloint@qq.com>
2022-04-23 11:18:50 +0800

    change style

--- js/common.js
+++ js/common.js
@@ -6,101 +6,39 @@ const pointsInfo = [];
  * @param lng 经度
  * @param lat 纬度
  */
-function addPosition(text, lng, lat) {
-	var point = new BMap.Point(lng, lat);
-	var marker = new BMap.Marker(point); // 创建标注
-	var offsetIndex = getOffsetIndex(text);
-	marker.setIcon(new BMap.Icon('../images/tooltip.gif',
-			new BMap.Size(34, 24),
-			{
-				anchor: new BMap.Size(7 + 3 * (text.length - 1), 24),
-				imageOffset: new BMap.Size(offsetIndex * -34, (text.length - 1) * -30)
-			}
-	));
-	var label = new BMap.Label(text, {offset: new BMap.Size(3, 0)});
+function addPosition(text, lng, lat, dataType0, dataType1, styleType) {
+	var point = new BMapGL.Point(lng, lat);
+	var marker = new BMapGL.Marker(point);
+	var textSize = styleType ? 14 : 20;
+	// 没办法去除icon，所以否则只能搞个透明的png取代
+	var icon = new BMapGL.Icon('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAAtJREFUGFdjYAACAAAFAAGq1chRAAAAAElFTkSuQmCC', new BMapGL.Size(textSize, textSize));
+	marker.setIcon(icon);
+	var offsetFix = [-3, -3];
+	var label = new BMapGL.Label(text, {offset: new BMapGL.Size(-textSize / 2 + offsetFix[0], -textSize / 2 + offsetFix[1])});
+	var color = colors1[dataType0];
 	label.setStyle({
-		background: 'none', color: '#fff', border: 'none'
+		color: '#fff',
+		width: textSize + 'px',
+		textAlign: 'center',
+		borderColor: dataType1 ? colors2[dataType1] : colors1[dataType0],
+		borderWidth: '2px',
+		borderRadius: '5px',
+		backgroundColor: color,
+		lineHeight: textSize + 'px'
 	});
 	marker.setLabel(label);
-	map.addOverlay(marker);
-}
-
-function addBlocked(text, lng, lat, type) {
-	var point = new BMap.Point(lng, lat);
-	var marker = new BMap.Marker(point); // 创建标注
-	var icon = type === 0 ? '../images/red-square.png' : '../images/black-square.png'
-	marker.setIcon(new BMap.Icon(icon,
-		new BMap.Size(32, 32),
-		{
-			anchor: new BMap.Size(14, 28),
-		}
-	));
-	var label = new BMap.Label(text, {offset: new BMap.Size(3, 0)});
-	label.setStyle({
-		background: 'none', color: '#fff', border: 'none'
-	});
-	marker.setLabel(label);
-	map.addOverlay(marker);
-}
-
-function addSamples(text, lng, lat, type) {
-	var point = new BMap.Point(lng, lat);
-	var marker = new BMap.Marker(point); // 创建标注
-	var icon = type === 0 ? '../images/red-square_s.png' : '../images/black-square_s.png'
-	marker.setIcon(new BMap.Icon(icon,
-		new BMap.Size(20, 20),
-		{
-			anchor: new BMap.Size(6, 24),
-		}
-	));
-	var label = new BMap.Label(text, {offset: new BMap.Size(3, 0)});
-	label.setStyle({
-		background: 'none', color: 'red', border: 'none'
-	});
-	marker.setLabel(label);
-	map.addOverlay(marker);
-}
-
-function addSamples2(text, lng, lat) {
-	var point = new BMap.Point(lng, lat);
-	var marker = new BMap.Marker(point); // 创建标注
-	marker.setIcon(new BMap.Icon('../images/black-square_s.png',
-		new BMap.Size(20, 20),
-		{
-			anchor: new BMap.Size(6, 24),
-		}
-	));
-	var label = new BMap.Label(text, {offset: new BMap.Size(3, 0)});
-	label.setStyle({
-		background: 'none', color: 'red', border: 'none'
-	});
-	marker.setLabel(label);
-	map.addOverlay(marker);
-}
-
-function addTestMarker() {
-	var myIcon = new BMap.Icon('http://api.map.baidu.com/img/markers.png', new BMap.Size(23, 25), {
-		// 指定定位位置。
-		// 当标注显示在地图上时，其所指向的地理位置距离图标左上
-		// 角各偏移10像素和25像素。您可以看到在本例中该位置即是
-		// 图标中央下端的尖角位置。
-		anchor: new BMap.Size(10, 25),
-		// 设置图片偏移。
-		// 当您需要从一幅较大的图片中截取某部分作为标注图标时，您
-		// 需要指定大图的偏移位置，此做法与css sprites技术类似。
-		imageOffset: new BMap.Size(0, 0 - 11 * 25)   // 设置图片偏移
-	});
-	var point = new BMap.Point(...testMarkerPosition);
-	// 创建标注对象并添加到地图
-	var marker = new BMap.Marker(point, {icon: myIcon});
+	// FIXME: doesn't work
+	// marker.addEventListener('hover mouseenter', function (e) {
+	// 	marker.setZIndex(currMakerZIndex++);
+	// });
 	map.addOverlay(marker);
 }
 
 // 小区轮廓
 function drawOutline() {
-	var outlinePolygon = new BMap.Polygon(outlinePoints.map((position) => {
-		return new BMap.Point(...position);
-	}), {strokeColor: 'blue', strokeWeight: 1, strokeOpacity: 0, fillColor: 'none'});
+	var outlinePolygon = new BMapGL.Polygon(outlinePoints.map((position) => {
+		return new BMapGL.Point(...position);
+	}), {strokeColor: 'blue', strokeWeight: 1, strokeOpacity: 0.5, fillColor: 'blue', fillOpacity: 0.05});
 	map.addOverlay(outlinePolygon);
 }
 
@@ -108,10 +46,10 @@ function drawOutline() {
 function drawTitle() {
 	if (mapTitle) {
 		var titleOpts = {
-			position: new BMap.Point(...titlePosition),
-			offset: new BMap.Size(0, 0),
+			position: new BMapGL.Point(...mapTitlePosition),
+			offset: new BMapGL.Size(0, 0),
 		};
-		var titleLabel = new BMap.Label(mapTitle, titleOpts);
+		var titleLabel = new BMapGL.Label(mapTitle, titleOpts);
 		titleLabel.setStyle({
 			color: 'gray',
 			borderRadius: '5px',
@@ -128,11 +66,10 @@ function drawTitle() {
 function printAuthor() {
 	if (mapAuthor) {
 		var authorOpts = {
-			position: new BMap.Point(...authorPosition),
-			offset: new BMap.Size(0, 0) // 设置文本偏移量
+			position: new BMapGL.Point(...mapAuthorPosition),
+			offset: new BMapGL.Size(0, 0) // 设置文本偏移量
 		};
-		var authorLabel = new BMap.Label(mapAuthor, authorOpts);
-		// 自定义文本标注样式
+		var authorLabel = new BMapGL.Label(mapAuthor, authorOpts);
 		authorLabel.setStyle({
 			color: 'gray',
 			borderRadius: '5px',
@@ -147,8 +84,8 @@ function printAuthor() {
 // 版本号
 function printVersion() {
 	var versionOpts = {
-		position: new BMap.Point(...versionPosition),
-		offset: new BMap.Size(0, 0) // 设置文本偏移量
+		position: new BMapGL.Point(...versionPosition),
+		offset: new BMapGL.Size(0, 0) // 设置文本偏移量
 	};
 	var now = new Date();
 	var year = now.getFullYear();
@@ -157,8 +94,7 @@ function printVersion() {
 	var day = now.getDate() < 10 ? '0' + now.getDate() : now
 			.getDate();
 	var version = [versionPrefix, year, month, day, '.', now.getHours()].join('');
-	var versionLabel = new BMap.Label(version, versionOpts);
-	// 自定义文本标注样式
+	var versionLabel = new BMapGL.Label(version, versionOpts);
 	versionLabel.setStyle({
 		color: 'gray',
 		borderRadius: '5px',
@@ -173,20 +109,25 @@ function printVersion() {
 }
 
 function initMap() {
-	var defaultMapType = map.getMapType();
-	var defaultTileLayer = defaultMapType.getTileLayer();
-	var newMapType = new BMap.MapType('新地图', defaultTileLayer, {minZoom: 15, maxZoom: 25});
-	map.setMapType(newMapType);
-	var point = new BMap.Point(...mapPosition);
+	if (mapTitle) {
+		document.title = mapTitle;
+	}
+
+	var point = new BMapGL.Point(...mapPosition);
 	map.centerAndZoom(point, mapZoom); // 设置中心点坐标和地图级别
-	map.setCurrentCity('上海'); // 设置地图显示的城市
 	map.enableScrollWheelZoom(true); // 开启鼠标滚轮缩放
+	map.setZoom(mapZoom);
 
 	map.addEventListener('click', function (e) {
-		pointsInfo.push(`['', ${e.point.lng}, ${e.point.lat}],`);
+		pointsInfo.push(`['', ${e.latlng.lng}, ${e.latlng.lat}],`);
 	});
 
 	map.addEventListener('dblclick', function (e) {
 		e.stopPropagation();
+	});
+
+	map.addEventListener("zoomend", function () {
+		var currZoom = this.getZoom();
+		console.log(`currentZoom: ${currZoom}`);
 	});
 }
